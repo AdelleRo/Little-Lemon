dd626c6b512ae92f4f9577eeb668ce0c62978157
AdelleRo <adelinedewet424@gmail.com>
2025-04-03 12:57:59 +0200

    started reservation page

--- src/components/Reservations.js
+++ src/components/Reservations.js
@@ -2,9 +2,22 @@
 
 const Reservations = () => {
     return (
-        <header>
-            <h1>Reservations</h1>
-        </header>
+        <div className="reservation-page">
+            <div className="reservation-form-container">
+                <h1 className="markazi-display-title reservation-title">Reservation</h1>
+                <form className="reservation-form">
+
+                </form>
+            </div>
+            <div className="gallery-and-btn">
+                <div class="gallery">
+                    <img alt="gallery"/>
+                    <img alt="gallery"/>
+                    <img alt="gallery"/>
+                </div>
+                {/* reserve a table button */}
+            </div>
+        </div>
     )
 }
 
